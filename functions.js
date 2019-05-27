9afdb654d8affd4fbd78a38a8262e82543d94602
Paweł Nobis <pawel.nobis1@gmail.com>
2019-05-27 09:59:58 +0200

    add closures

--- functions.js
+++ functions.js
@@ -1,3 +1,82 @@
-function example() {
-    console.log('hello world');
+const getData = function () {
+    console.log('getting data');
+    return fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results[0])
+            const person = data.results[0];
+            let first_name = person.name.first;
+            first_name = first_name.charAt(0).toUpperCase() + first_name.slice(1);
+            let last_name = person.name.last;
+            last_name = last_name[0].toUpperCase() + last_name.slice(1);
+            let photo = person.picture.large;
+
+            const wrapper = document.querySelector('.person');
+            const image = document.createElement('img');
+            image.classList.add('person__image');
+            image.src = person.picture.large;
+            const personalData = document.createElement('h3');
+            personalData.classList.add('person__data');
+            personalData.innerText = `${first_name} ${last_name}`;
+            wrapper.appendChild(image);
+            wrapper.appendChild(personalData);
+            console.log(personalData);
+
+        })
+        .catch(err => console.log(err.message));
 }
+
+const search = function () {
+    let level = 1
+    console.log(`Searching the dungeoun - level ${level}...`);
+    return function () {
+        level++;
+        console.log(`Searching the dungeoun - level ${level}...`);
+        return function () {
+            level++;
+            console.log(`Searching the dungeoun - level ${level}...`);
+            return function () {
+                level++;
+                console.log(`Searching the dungeoun - level ${level}...`);
+                return function () {
+                    level++;
+                    console.log(`Searching the dungeoun - level ${level}...`);
+                    return function () {
+                        level++;
+                        console.log(`Searching the dungeoun - level ${level}...`);
+                        return function () {
+                            level++;
+                            console.log(`Searching the dungeoun - level ${level}...`);
+                            return function () {
+                                level++;
+                                console.log(`Searching the dungeoun - level ${level}...`);
+                                return function () {
+                                    level++;
+                                    console.log(`Searching the dungeoun - level ${level}...`);
+                                    return function () {
+                                        level++;
+                                        console.log(`Searching the dungeoun - level ${level}...`);
+                                        setTimeout(() => {
+                                            console.log('You\'ve found the treasure!');
+                                        }, 1000);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+const lvl1 = search();
+const lvl2 = lvl1();
+const lvl3 = lvl2();
+const lvl4 = lvl3();
+const lvl5 = lvl4();
+const lvl6 = lvl5();
+const lvl7 = lvl6();
+const lvl8 = lvl7();
+const lvl9 = lvl8();
+lvl9();
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-example();
+const searchBtn = document.querySelector('.btn--search');
+
+// getData();
+
+// searchBtn.addEventListener('click', search);
